5c86550503635e033971aaca2189205b12c2a90c
shwetha <sn@sparkcognition.com>
2026-04-15 08:27:59 +0530

    Solved contain_duplicates problem

--- Two_Sum/two_sum.cpp
+++ Two_Sum/two_sum.cpp
@@ -30,6 +30,46 @@ public:
 };
 
 // Time complexity O(n^2)
+// Space complexity O(1)
+
+
+
+----------------------------------------------------------------------
+                       BETTER APPROACH
+----------------------------------------------------------------------
+
+class Solution 
+{
+    public:
+        vector<int> twoSum(vector<int> arr, int target) 
+        {
+            int n = arr.size();
+            vector<pair<int, int>> numsWithIndex;
+            for (int i = 0; i < n; i++) 
+            {
+                numsWithIndex.push_back({arr[i], i});
+            }
+            sort(numsWithIndex.begin(), numsWithIndex.end());
+
+            int left = 0, right = n - 1;
+            while (left < right) 
+            {
+                int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+                if (sum == target) 
+                    // Return original indices
+                    return {numsWithIndex[left].second, numsWithIndex[right].second};
+                else if (sum < target) 
+                    left++;
+                else 
+                    right--;                
+            }
+            return {}; 
+        }
+};
+
+// Time complexity O(nlogn)
+// Space complexity O(n)
+
 
 
 ----------------------------------------------------------------------
@@ -59,6 +99,11 @@ public:
 };
 
 // Time complexity O(n)
+// Space complexity O(n)
+
+
+
+
 
 
 
--- Two_Sum/two_sum.py
+++ Two_Sum/two_sum.py
@@ -12,15 +12,48 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
                        BRUTE FORCE APPROACH
 ----------------------------------------------------------------------
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0,len(nums)-1):
-            for j in range((i+1), len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(0,len(nums)-1):
+        for j in range((i+1), len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
                 
 
 # Time complexity O(n^2)
+# Space complexity O(1)
+
+
+
+----------------------------------------------------------------------
+                       BETTER APPROACH
+----------------------------------------------------------------------
+def two_sum_indices(self, arr, target):
+        nums_with_index = [(num, idx) for idx, num in enumerate(arr)]
+        
+        # Sort the list by values
+        nums_with_index.sort(key=lambda x: x[0])
+
+        left, right = 0, len(arr) - 1
+        
+        while left < right:
+            current_sum = nums_with_index[left][0] + nums_with_index[right][0]
+            if current_sum == target:
+                # Return original indices of found elements
+                return [nums_with_index[left][1], nums_with_index[right][1]]
+            elif current_sum < target:
+                # Move left pointer right to increase sum
+                left += 1
+            else:
+                # Move right pointer left to decrease sum
+                right -= 1
+    
+        return [-1, -1]
+
+
+# Time complexity O(nlogn)
+# Space complexity O(n)
+
 
 
 ----------------------------------------------------------------------
@@ -41,6 +74,8 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
 
 
 # Time complexity O(n)
+# Space complexity O(n)
+
 
 
 
